2a2582afd82badc6242c8ce65db3b31ec604f8c0
BeknurSM <108877381+BeknurSM@users.noreply.github.com>
2022-12-13 13:15:16 +0600

    Update Plane.java

--- src/main/java/com/epam/training/student_beknur_smagulov/cleancode/planes/src/main/java/planes/Plane.java
+++ src/main/java/com/epam/training/student_beknur_smagulov/cleancode/planes/src/main/java/planes/Plane.java
@@ -36,22 +36,6 @@ abstract public class Plane {
                 ", maxSpeed=" + maxSpeed +
                 ", maxFlightDistance=" + maxFlightDistance +
                 ", maxLoadCapacity=" + maxLoadCapacity +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Plane)) return false;
-        Plane plane = (Plane) o;
-        return maxSpeed == plane.maxSpeed &&
-                maxFlightDistance == plane.maxFlightDistance &&
-                maxLoadCapacity == plane.maxLoadCapacity &&
-                Objects.equals(model, plane.model);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(model, maxSpeed, maxFlightDistance, maxLoadCapacity);
+                '}';      
     }
 }
